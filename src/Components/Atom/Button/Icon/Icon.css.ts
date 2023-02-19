45e5dd0c6fb2dc78a809b2af5cfab8ae0d9e69a7
Alex Knight <alexander.knight@boughtbymany.com>
2023-02-19 04:13:34 +0000

    added Atom Button styling

--- src/Components/Atom/Button/Icon/Icon.css.ts
+++ src/Components/Atom/Button/Icon/Icon.css.ts
@@ -1,3 +1,12 @@
 import { style } from '@vanilla-extract/css'
 
-export const container = style({})
+import { variable } from '@style/reset.css'
+
+export const container = style({
+  border: 0,
+  borderRadius: 4,
+  height: 20,
+  marginLeft: variable.spacing.m,
+  padding: 0,
+  width: 20
+})
