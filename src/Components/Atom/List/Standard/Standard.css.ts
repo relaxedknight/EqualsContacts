57a4451eabf7d6c851ab15b699aecaad23daaaf9
Alex Knight <alexander.knight@boughtbymany.com>
2023-02-19 04:13:50 +0000

    Added Atom List styling

--- src/Components/Atom/List/Standard/Standard.css.ts
+++ src/Components/Atom/List/Standard/Standard.css.ts
@@ -1,7 +1,18 @@
 import { style } from '@vanilla-extract/css'
 
+import { variable } from '@style/reset.css'
+
 export const container = style({
   listStyle: 'none'
 })
 
-export const item = style({})
+export const item = style({
+  'selectors': {
+    [`${container} > &`]: {
+      marginBottom: variable.spacing.m
+    },
+    [`${container} > &:last-child`]: {
+      marginBottom: 0
+    }
+  }
+})
