1a332af92e33c0d154ca863c9eb868d07145bb99
Alex Knight <alexander.knight@boughtbymany.com>
2023-02-20 14:30:19 +0000

    resolves a build step bug

--- src/style/font.css.ts
+++ src/style/font.css.ts
@@ -1,11 +1,9 @@
 import { fontFace } from '@vanilla-extract/css'
 
-import * as font from './font'
-
 export const EqualsBold = fontFace({
-  src: `url(${font.EqualsBold})`
+  src: 'url(/font/equals-bold.woff2)'
 })
 
 export const EqualsLight = fontFace({
-  src: `url(${font.EqualsLight})`
+  src: 'url(/font/equals-light.woff2)'
 })
